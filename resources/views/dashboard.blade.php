4850852f992179001ff232de7a7235a996b50077
lastpeace <caesarios722@gmail.com>
2023-05-22 01:06:45 +0700

    kelar bang buat hari ini

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Relasi One to One & Many To Many</title>
+    <title>Aplikasi Evoting</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
         crossorigin="anonymous">
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,17 @@
 @extends('layouts.app2')
 
 <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
 @section('content')
     <div class="jumbotron" name="header" style="text-align: center;">
         <h1>Website Voting Untuk Tugas Pemweb2</h1>
         <p>Selamat Datang diwebsite E-VOTING</p>
-        <p> 
-            <button class="button-54"><a class="btn btn-primary btn-lg" style="color: black; " href="{{ route('login') }}" role="button">Login</a></button>
-            <button class="button-54"><a class="btn btn-success btn-lg" style="color: black; " href="{{ route('register') }}" role="button">Daftar</a></button>
+        <p>
+            <button class="button-54"><a class="btn btn-primary btn-lg" style="color: black; " href="{{ route('login') }}"
+                    role="button">Login</a></button>
+            <button class="button-54"><a class="btn btn-success btn-lg" style="color: black; "
+                    href="{{ route('register') }}" role="button">Daftar</a></button>
         </p>
         </form>
     </div>
@@ -19,71 +21,68 @@
 
 <!-- HTML !-->
 <style>
+    .button-54 {
+        font-family: "Open Sans", sans-serif;
+        font-size: 16px;
+        letter-spacing: 2px;
+        text-decoration: none;
+        text-transform: uppercase;
+        color: #000;
+        cursor: pointer;
+        border: 3px solid;
+        padding: 0.25em 0.5em;
+        box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;
+        position: relative;
+        user-select: none;
+        -webkit-user-select: none;
+        touch-action: manipulation;
+    }
 
+    .button-54:active {
+        box-shadow: 0px 0px 0px 0px;
+        top: 5px;
+        left: 5px;
+    }
 
-.button-54 {
-  font-family: "Open Sans", sans-serif;
-  font-size: 16px;
-  letter-spacing: 2px;
-  text-decoration: none;
-  text-transform: uppercase;
-  color: #000;
-  cursor: pointer;
-  border: 3px solid;
-  padding: 0.25em 0.5em;
-  box-shadow: 1px 1px 0px 0px, 2px 2px 0px 0px, 3px 3px 0px 0px, 4px 4px 0px 0px, 5px 5px 0px 0px;
-  position: relative;
-  user-select: none;
-  -webkit-user-select: none;
-  touch-action: manipulation;
-}
+    @media (min-width: 768px) {
+        .button-54 {
+            padding: 0.25em 0.75em;
+        }
+    }
 
-.button-54:active {
-  box-shadow: 0px 0px 0px 0px;
-  top: 5px;
-  left: 5px;
-}
+    /* public/css/welcome.css */
+    body {
+        background-color: #f7f7f7;
+        font-family: Arial, sans-serif;
+    }
 
-@media (min-width: 768px) {
-  .button-54 {
-    padding: 0.25em 0.75em;
-  }
-}
+    .container {
+        align-content: center;
+        height: 100%;
+        max-width: 800px;
+        margin: 0 auto;
+        justify-content: center;
+        align-items: center;
+        display: flex;
 
-/* public/css/welcome.css */
-body {
-    background-color: #f7f7f7;
-    font-family: Arial, sans-serif;
-}
+    }
 
-.container {
-    align-content: center;
-    height: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-   justify-content: center;
-   align-items: center;
-   display: flex;
-    
-}
-.jumbotron {
-    background-color: grey;
-    border-radius: 10px;
-    padding: 2rem;
-    box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
-}
+    .jumbotron {
+        background-color: grey;
+        border-radius: 10px;
+        padding: 2rem;
+        box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+    }
 
-.jumbotron h1 {
-    font-size: 2.5rem;
-    margin-bottom: 1rem;
-    color: black;
-}
+    .jumbotron h1 {
+        font-size: 2.5rem;
+        margin-bottom: 1rem;
+        color: black;
+    }
 
-.jumbotron p {
-    font-size: 1.2rem;
-    margin-bottom: 2rem;
-    color: black;
-}
-
-
+    .jumbotron p {
+        font-size: 1.2rem;
+        margin-bottom: 2rem;
+        color: black;
+    }
 </style>
