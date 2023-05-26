b528515a74790ef2d560df203e43715f29d53286
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-05-26 11:16:17 +0700

    ngubah icon

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="icon" href="{{ asset('/Source') }}/evot.jpg">
+    <link rel="icon" href="{{ asset('/Source') }}/favicon.ico">
         <title>Aplikasi Evoting</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
             crossorigin="anonymous">
