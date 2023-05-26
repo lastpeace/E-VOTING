ef7467ea62c83fa001c2e4f0252fcd76261549bb
lastpeace <caesarios722@gmail.com>
2023-05-26 10:12:30 +0700

    ulang kadidat

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,49 +1,48 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Aplikasi Evoting</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        crossorigin="anonymous">
-</head>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Aplikasi Evoting</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+            crossorigin="anonymous">
+    </head>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Dashboard') }}
+            </h2>
+        </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <p>Welcome, {{ Auth::user()->name }}!</p>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <p>Welcome, {{ Auth::user()->name }}!</p>
+                    </div>
                 </div>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nama User</th>
+                            <th>Roles</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{{ Auth::user()->name }}</td>
+                            <td>
+                                @foreach (Auth::user()->roles()->get() as $role)
+                                    <button class="btn btn-sm btn-primary me-2">{{ $role->name }}</button>
+                                @endforeach
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <td>Tambah Data</td>
+                {{-- <td><button class="btn btn-sm btn-primary me-2"href="{{ route('candidates.create') }}">TAMBAH</button> --}}
+                </td>
+
             </div>
-            <table class="table table-bordered table-striped">
-                <thead>
-                    <tr>
-                        <th>Nama User</th>
-                        <th>Roles</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>{{ Auth::user()->name }}</td>
-                        <td>
-                            @foreach (Auth::user()->roles()->get() as $role)
-                                <button class="btn btn-sm btn-primary me-2">{{ $role->name }}</button>
-                            @endforeach
-                        </td>
-                    </tr>
-                </tbody>
-
-            </table>
-            <td>Tambah Data</td>
-            <td><button class="btn btn-sm btn-primary me-2"href="{{ route('candidates.create') }}">TAMBAH</button>
-            </td>
-
         </div>
-    </div>
 
 
-</x-app-layout>
+    </x-app-layout>
