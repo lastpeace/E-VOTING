1aa3612170ddf2e2362c4f9a07bae7717506c7a6
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-06-07 23:54:08 +0700

    dashboard dikit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,15 +6,9 @@
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"> -->
     </head>
     <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Dashboard') }}
-            </h2>
-        </x-slot>
-
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="py-2">
+            <div class="mx-auto sm:px-4 lg:px-4 lg">
+                <div class="h-full rounded-t-3xl bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <p>Welcome, {{ Auth::user()->name }}!</p>
                     </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,17 +7,17 @@
 <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 @section('content')
 <div class="grid h-screen place-items-center">
-    <div class="w-1/2 m-auto bg-white h-1/2 text-center rounded-lg shadow-2xl grid place-items-center" name="header">
-        <div class="w-full font-sans">
-            <h1 class="font-bold sm:text-2xl md:text-3xl lg:text-4xl xl:text-6xl">Website <span
+    <div class="w-3/4 my-1/2 bg-white h-1/2 text-center rounded-lg shadow-2xl flex items-center justify-center" name="header">
+        <div class="w-full font-sans px-8">
+            <h1 class="font-bold text-3xl sm:text-4xl md:text-6xl lg:text-6xl xl:text-6xl">Website <span
                     class="font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">E-Voting</span>
                 Untuk Tugas Pemrograman Web
                 II</h1>
-            <p class="mb-4 mt-4">Selamat Datang diwebsite E-VOTING</p>
-            <div class="">
-                <a class=" bg-slate-300 py-2 px-4 rounded-full shadow-md hover:bg-slate-950 hover:text-white hover:py-2.5 hover:px-8 hover:text-lg hover:shadow-xl transition-colors"
+            <p class="mb-2 mt-2">Selamat Datang di Website E-VOTING</p>
+            <div class="p-2">
+                <a class="bg-slate-300 py-2 px-4 rounded-full shadow-md hover:bg-slate-950 hover:text-white hover:px-6 hover:py-2.5 hover:shadow-xl hover:font-bold transition-all"
                     href="{{ route('login') }}" role="button">
-                    Login
+                    {{ __('Login') }}
                 </a>
             </div>
         </div>
