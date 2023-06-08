1cfb82b759a8c6942385935d36aeba07367d767b
lastpeace <caesarios722@gmail.com>
2023-06-09 00:14:32 +0700

    Role

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,9 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,16 +46,4 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-<<<<<<< HEAD
-    public function assignRole($admin)
-    {
-        $this->roles()->attach($admin);
-    }
-
 }
-=======
-    /**public function roles(){
-        return $this->belongsToMany(Role::class, 'user_role');
-    }*/
-}
->>>>>>> f9148a0e56d3279464521ccfaffe40de7815530c
