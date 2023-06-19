befef6070a3934b438da92a4a569c79197fb0032
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-06-19 14:32:59 +0700

    seedernya ganti

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,70 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(2)->has(
-            Kelas::factory()->count(2)
-        )->create();
+        // User::factory(2)->has(
+        //     Kelas::factory()->count(2)
+        // )->create();
+        Kelas::create([
+            'nama_kelas' => 'admin'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 1'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 2'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 3'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 4'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 5'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 6'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X MIPA 7'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X IPS 1'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X IPS 2'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X IPS 3'
+        ]);
+        Kelas::create([
+            'nama_kelas' => 'X IPS 4'
+        ]);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin12345',
+            'role' => 'admin',
+            'kelas_id' => '1',
+        ]);
+        User::create([
+            'name' => 'Aufa',
+            'email' => 'aufa@email.com',
+            'password' => 'user12345',
+            'kelas_id' => '2',
+        ]);
+        User::create([
+            'name' => 'Azizi',
+            'email' => 'azizi@email.com',
+            'password' => 'user12345',
+            'kelas_id' => '3',
+        ]);
+        User::create([
+            'name' => 'Ammar',
+            'email' => 'ammar@email.com',
+            'password' => 'user12345',
+            'kelas_id' => '4',
+        ]);
     }
 
 
